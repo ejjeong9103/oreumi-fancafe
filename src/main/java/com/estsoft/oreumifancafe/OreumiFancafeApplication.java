36fd706b9b87c740d650b5b5942901a571d0f56f
ejjeong9103 <ejjeong9103@naver.com>
2024-10-30 09:31:11 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/estsoft/oreumifancafe/OreumiFancafeApplication.java
@@ -0,0 +1,13 @@
+package com.estsoft.oreumifancafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OreumiFancafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OreumiFancafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estsoft/oreumifancafe/OreumiFancafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estsoft.oreumifancafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OreumiFancafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
