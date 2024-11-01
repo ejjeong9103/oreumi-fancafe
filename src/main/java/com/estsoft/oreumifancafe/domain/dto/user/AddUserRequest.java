e9c3fa31d6b94e302f6d12e14cd09535bc118cd3
WeeYoungSeok <dndrldzk@gmail.com>
2024-11-01 16:27:11 +0900

    feat: 회원가입 필수 입력값 validate 기능 구현

--- src/main/java/com/estsoft/oreumifancafe/domain/dto/user/AddUserRequest.java
+++ src/main/java/com/estsoft/oreumifancafe/domain/dto/user/AddUserRequest.java
@@ -4,6 +4,8 @@ import com.estsoft.oreumifancafe.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.lang.reflect.Field;
+
 @Getter
 @AllArgsConstructor
 public class AddUserRequest {
@@ -24,5 +26,29 @@ public class AddUserRequest {
                 .addressDetail(this.addressDetail)
                 .email(this.email)
                 .build();
+    }
+
+    public void validate()  {
+        if (this.userId == null || this.userId.isEmpty()) {
+            throw new IllegalArgumentException("아이디는 필수 입력값입니다.");
+        }
+        if (this.userPw == null || this.userPw.isEmpty()) {
+            throw new IllegalArgumentException("비밀번호는 필수 입력값입니다.");
+        }
+        if (this.nickName == null || this.nickName.isEmpty()) {
+            throw new IllegalArgumentException("닉네임은 필수 입력값입니다.");
+        }
+        if (this.post == null || this.post.isEmpty()) {
+            throw new IllegalArgumentException("우편번호는 필수 입력값입니다.");
+        }
+        if (this.address == null || this.address.isEmpty()) {
+            throw new IllegalArgumentException("주소는 필수 입력값입니다.");
+        }
+        if (this.addressDetail == null || this.addressDetail.isEmpty()) {
+            throw new IllegalArgumentException("상세 주소는 필수 입력값입니다.");
+        }
+        if (this.email == null || this.email.isEmpty()) {
+            throw new IllegalArgumentException("이메일은 필수 입력값입니다.");
+        }
     }
 }
--- src/main/java/com/estsoft/oreumifancafe/service/user/UserService.java
+++ src/main/java/com/estsoft/oreumifancafe/service/user/UserService.java
@@ -2,14 +2,21 @@ package com.estsoft.oreumifancafe.service.user;
 
 import com.estsoft.oreumifancafe.domain.dto.user.AddUserRequest;
 import com.estsoft.oreumifancafe.domain.dto.user.UserResponse;
+import com.estsoft.oreumifancafe.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
 
     // 회원가입
     public UserResponse saveUser(AddUserRequest addUserRequest) {
         // 유저 정보 확인
+        // 빈값 확인
+        addUserRequest.validate();
         return null;
     }
 }
