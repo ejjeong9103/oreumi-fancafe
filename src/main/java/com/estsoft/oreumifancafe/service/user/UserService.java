0d1f4e2a8bc47a67702a5912b9f89c83e942f8d3
WeeYoungSeok <dndrldzk@gmail.com>
2024-11-01 16:51:18 +0900

    feat: 아이디 중복체크 로직 구현

--- src/main/java/com/estsoft/oreumifancafe/service/user/UserService.java
+++ src/main/java/com/estsoft/oreumifancafe/service/user/UserService.java
@@ -17,6 +17,20 @@ public class UserService {
         // 유저 정보 확인
         // 빈값 확인
         addUserRequest.validate();
+
+        // 아이디 중복 체크
+        duplicateUserId(addUserRequest.getUserId());
         return null;
+    }
+
+    // 아이디 중복 체크
+    public boolean isDuplicateUserId(String userId) {
+        return userRepository.existsByUserId(userId);
+    }
+
+    public void duplicateUserId(String userId) {
+        if (isDuplicateUserId(userId)) {
+            throw new IllegalArgumentException("중복된 아이디입니다.");
+        }
     }
 }
