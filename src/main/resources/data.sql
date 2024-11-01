ad1aa08e5fa95e961737f34a0d79c32e91266434
cjy <cjiyoon1130@gmail.com>
2024-11-01 13:06:48 +0900

    feat: user, help 테이블에 alter문으로 컬럼 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,3 @@
-DROP TABLE IF EXISTS `rank_up_board`;
-
-CREATE TABLE `rank_up_board` (
-                                 `id`	bigint	NOT NULL,
-                                 `title`	text	NOT NULL,
-                                 `content`	text	NOT NULL,
-                                 `rank_up_state`	int	NOT NULL	DEFAULT 1,
-                                 `created_at`	timestamp	NOT NULL	DEFAULT now(),
-                                 `updated_at`	timestamp	NOT NULL	DEFAULT now(),
-                                 `rank_id`	int	NOT NULL,
-                                 `user_id`	varchar(100)	NOT NULL
-);
-
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
@@ -25,290 +12,123 @@ CREATE TABLE `user` (
                         `profile_image_address`	varchar(2000)	NOT NULL
 );
 
-DROP TABLE IF EXISTS `free_board`;
+DROP TABLE IF EXISTS `board`;
 
-CREATE TABLE `free_board` (
+CREATE TABLE `board` (
+                         `id`	bigint	NOT NULL,
+                         `title`	text	NOT NULL,
+                         `content`	text	NOT NULL,
+                         `created_at`	timestamp	NOT NULL	DEFAULT now(),
+                         `updated_at`	timestamp	NOT NULL	DEFAULT now(),
+                         `user_id`	varchar(100)	NOT NULL,
+                         `board_type`	int	NOT NULL,
+                         `board_category_name`	varchar(100)	NULL,
+                         `state`	int	NULL
+);
+
+DROP TABLE IF EXISTS `help`;
+
+CREATE TABLE `help` (
+                        `id`	bigint	NOT NULL,
+                        `title`	text	NOT NULL,
+                        `content`	text	NOT NULL,
+                        `created_at`	timestamp	NOT NULL	DEFAULT now(),
+                        `user_id`	varchar(100)	NOT NULL,
+                        `admin_id`	varchar(100)	NULL,
+                        `answer`	text	NULL,
+                        `state`	int	NOT NULL,
+                        `answered_at`	timestamp	NULL
+);
+
+DROP TABLE IF EXISTS `reply`;
+
+CREATE TABLE `reply` (
+                         `id`	bigint	NOT NULL,
+                         `content`	text	NOT NULL,
+                         `created_at`	timestamp	NOT NULL	DEFAULT now(),
+                         `updated_at`	timestamp	NOT NULL	DEFAULT now(),
+                         `hierarchy`	int	NOT NULL,
+                         `orders`	int	NOT NULL,
+                         `group`	int	NOT NULL,
+                         `board_id`	bigint	NOT NULL,
+                         `user_id`	varchar(100)	NOT NULL
+);
+
+DROP TABLE IF EXISTS `board_category`;
+
+CREATE TABLE `board_category` (
+                                  `id`	bigint	NOT NULL,
+                                  `board_category_name`	varchar(100)	NOT NULL,
+                                  `board_type_id`	bigint	NOT NULL
+);
+
+DROP TABLE IF EXISTS `board_type`;
+
+CREATE TABLE `board_type` (
                               `id`	bigint	NOT NULL,
-                              `title`	text	NOT NULL,
-                              `content`	text	NOT NULL,
-                              `created_at`	timestamp	NOT NULL	DEFAULT now(),
-                              `updated_at`	timestamp	NOT NULL	DEFAULT now(),
-                              `user_id`	varchar(100)	NOT NULL
+                              `board_name`	varchar(100)	NOT NULL,
+                              `board_type`	int	NOT NULL
 );
-
-DROP TABLE IF EXISTS `report_board`;
-
-CREATE TABLE `report_board` (
-                                `id`	bigint	NOT NULL,
-                                `title`	text	NOT NULL,
-                                `content`	text	NOT NULL,
-                                `created_at`	timestamp	NOT NULL	DEFAULT now(),
-                                `updated_at`	timestamp	NOT NULL	DEFAULT now(),
-                                `user_id`	varchar(100)	NOT NULL
-);
-
-DROP TABLE IF EXISTS `qa_board`;
-
-CREATE TABLE `qa_board` (
-                            `id`	bigint	NOT NULL,
-                            `title`	text	NOT NULL,
-                            `content`	text	NOT NULL,
-                            `created_at`	timestamp	NOT NULL	DEFAULT now(),
-                            `updated_at`	timestamp	NOT NULL	DEFAULT now(),
-                            `state`	int	NOT NULL	DEFAULT 1,
-                            `user_id`	varchar(100)	NOT NULL
-);
-
-DROP TABLE IF EXISTS `enjoy_board`;
-
-CREATE TABLE `enjoy_board` (
-                               `id`	bigint	NOT NULL	COMMENT '기본키',
-                               `content`	text	NOT NULL,
-                               `title`	text	NOT NULL,
-                               `created_att`	timestamp	NOT NULL	DEFAULT now(),
-                               `updated_at`	timestamp	NOT NULL	DEFAULT now(),
-                               `Field`	varchar(2000)	NULL,
-                               `user_id`	varchar(100)	NOT NULL
-);
-
-DROP TABLE IF EXISTS `community_board`;
-
-CREATE TABLE `community_board` (
-                                   `id`	bigint	NOT NULL,
-                                   `title`	text	NOT NULL,
-                                   `content`	text	NOT NULL,
-                                   `created_at`	timestamp	NOT NULL	DEFAULT now(),
-                                   `updated_att`	timestamp	NOT NULL	DEFAULT now(),
-                                   `user_id`	varchar(100)	NOT NULL
-);
-
-DROP TABLE IF EXISTS `notice_board`;
-
-CREATE TABLE `notice_board` (
-                                `id`	bigint	NOT NULL,
-                                `title`	text	NOT NULL,
-                                `content`	text	NOT NULL,
-                                `created_at`	timestamp	NOT NULL	DEFAULT now(),
-                                `updated_at`	timestamp	NOT NULL	DEFAULT now(),
-                                `user_id`	varchar(100)	NOT NULL
-);
-
-DROP TABLE IF EXISTS `rank`;
-
-CREATE TABLE `rank` (
-                        `id`	int	NOT NULL,
-                        `name`	varchar(20)	NOT NULL
-);
-
-DROP TABLE IF EXISTS `community_reply`;
-
-CREATE TABLE `community_reply` (
-                                   `id`	bigint	NOT NULL,
-                                   `content`	text	NOT NULL,
-                                   `created_at`	timestamp	NOT NULL	DEFAULT now(),
-                                   `updated_at`	timestamp	NOT NULL	DEFAULT now(),
-                                   `hierarchy`	int	NOT NULL,
-                                   `orders`	int	NOT NULL,
-                                   `group`	int	NOT NULL,
-                                   `community_id`	bigint	NOT NULL,
-                                   `user_id`	varchar(100)	NOT NULL
-);
-
-DROP TABLE IF EXISTS `qa_reply`;
-
-CREATE TABLE `qa_reply` (
-                            `id`	bigint	NOT NULL,
-                            `qa_id`	bigint	NOT NULL,
-                            `user_id`	varchar(100)	NOT NULL,
-                            `content`	text	NOT NULL,
-                            `created_at`	timestamp	NOT NULL	DEFAULT now(),
-                            `updated_at`	timestamp	NOT NULL	DEFAULT now(),
-                            `hierarchy`	int	NOT NULL,
-                            `orders`	int	NOT NULL,
-                            `group`	int	NOT NULL
-);
-
-DROP TABLE IF EXISTS `free_replay`;
-
-CREATE TABLE `free_replay` (
-                               `id`	bigint	NOT NULL,
-                               `content`	text	NOT NULL,
-                               `created_at`	timestamp	NOT NULL	DEFAULT now(),
-                               `updated_at`	timestamp	NOT NULL	DEFAULT now(),
-                               `hierarchy`	int	NOT NULL,
-                               `orders`	int	NOT NULL,
-                               `group`	int	NOT NULL,
-                               `free_id`	bigint	NOT NULL,
-                               `user_id`	varchar(100)	NOT NULL
-);
-
-DROP TABLE IF EXISTS `enjoy_reply`;
-
-CREATE TABLE `enjoy_reply` (
-                               `id`	bigint	NOT NULL,
-                               `content`	text	NOT NULL,
-                               `created_at`	timestamp	NOT NULL	DEFAULT now(),
-                               `updated_at`	timestamp	NOT NULL	DEFAULT now(),
-                               `hierarchy`	int	NOT NULL,
-                               `orders`	int	NOT NULL,
-                               `group`	int	NOT NULL,
-                               `enjoy_id`	bigint	NOT NULL,
-                               `user_id`	varchar(100)	NOT NULL
-);
-
-ALTER TABLE `rank_up_board` ADD CONSTRAINT `PK_RANK_UP_BOARD` PRIMARY KEY (
-                                                                           `id`
-    );
 
 ALTER TABLE `user` ADD CONSTRAINT `PK_USER` PRIMARY KEY (
                                                          `user_id`
     );
 
-ALTER TABLE `free_board` ADD CONSTRAINT `PK_FREE_BOARD` PRIMARY KEY (
-                                                                     `id`
+ALTER TABLE `board` ADD CONSTRAINT `PK_BOARD` PRIMARY KEY (
+                                                           `id`
     );
 
-ALTER TABLE `report_board` ADD CONSTRAINT `PK_REPORT_BOARD` PRIMARY KEY (
-                                                                         `id`
-    );
-
-ALTER TABLE `qa_board` ADD CONSTRAINT `PK_QA_BOARD` PRIMARY KEY (
-                                                                 `id`
-    );
-
-ALTER TABLE `enjoy_board` ADD CONSTRAINT `PK_ENJOY_BOARD` PRIMARY KEY (
-                                                                       `id`
-    );
-
-ALTER TABLE `community_board` ADD CONSTRAINT `PK_COMMUNITY_BOARD` PRIMARY KEY (
-                                                                               `id`
-    );
-
-ALTER TABLE `notice_board` ADD CONSTRAINT `PK_NOTICE_BOARD` PRIMARY KEY (
-                                                                         `id`
-    );
-
-ALTER TABLE `rank` ADD CONSTRAINT `PK_RANK` PRIMARY KEY (
+ALTER TABLE `help` ADD CONSTRAINT `PK_HELP` PRIMARY KEY (
                                                          `id`
     );
 
-ALTER TABLE `community_reply` ADD CONSTRAINT `PK_COMMUNITY_REPLY` PRIMARY KEY (
-                                                                               `id`
+ALTER TABLE `reply` ADD CONSTRAINT `PK_REPLY` PRIMARY KEY (
+                                                           `id`
     );
 
-ALTER TABLE `qa_reply` ADD CONSTRAINT `PK_QA_REPLY` PRIMARY KEY (
-                                                                 `id`
+ALTER TABLE `board_category` ADD CONSTRAINT `PK_BOARD_CATEGORY` PRIMARY KEY (
+                                                                             `id`
     );
 
-ALTER TABLE `free_replay` ADD CONSTRAINT `PK_FREE_REPLAY` PRIMARY KEY (
-                                                                       `id`
+ALTER TABLE `board_type` ADD CONSTRAINT `PK_BOARD_TYPE` PRIMARY KEY (
+                                                                     `id`
     );
 
-ALTER TABLE `enjoy_reply` ADD CONSTRAINT `PK_ENJOY_REPLY` PRIMARY KEY (
-                                                                       `id`
-    );
-
-ALTER TABLE `rank_up_board` ADD CONSTRAINT `FK_rank_TO_rank_up_board_1` FOREIGN KEY (
-                                                                                     `rank_id`
-    )
-    REFERENCES `rank` (
-                       `id`
-        );
-
-ALTER TABLE `rank_up_board` ADD CONSTRAINT `FK_user_TO_rank_up_board_1` FOREIGN KEY (
-                                                                                     `user_id`
+ALTER TABLE `board` ADD CONSTRAINT `FK_user_TO_board_1` FOREIGN KEY (
+                                                                     `user_id`
     )
     REFERENCES `user` (
                        `user_id`
         );
 
-ALTER TABLE `free_board` ADD CONSTRAINT `FK_user_TO_free_board_1` FOREIGN KEY (
-                                                                               `user_id`
+ALTER TABLE `help` ADD CONSTRAINT `FK_user_TO_help_1` FOREIGN KEY (
+                                                                   `user_id`
     )
     REFERENCES `user` (
                        `user_id`
         );
 
-ALTER TABLE `report_board` ADD CONSTRAINT `FK_user_TO_report_board_1` FOREIGN KEY (
-                                                                                   `user_id`
+ALTER TABLE `reply` ADD CONSTRAINT `FK_board_TO_reply_1` FOREIGN KEY (
+                                                                      `board_id`
+    )
+    REFERENCES `board` (
+                        `id`
+        );
+
+ALTER TABLE `reply` ADD CONSTRAINT `FK_user_TO_reply_1` FOREIGN KEY (
+                                                                     `user_id`
     )
     REFERENCES `user` (
                        `user_id`
         );
 
-ALTER TABLE `qa_board` ADD CONSTRAINT `FK_user_TO_qa_board_1` FOREIGN KEY (
-                                                                           `user_id`
+ALTER TABLE `board_category` ADD CONSTRAINT `FK_board_type_TO_board_category_1` FOREIGN KEY (
+                                                                                             `board_type_id`
     )
-    REFERENCES `user` (
-                       `user_id`
-        );
-
-ALTER TABLE `enjoy_board` ADD CONSTRAINT `FK_user_TO_enjoy_board_1` FOREIGN KEY (
-                                                                                 `user_id`
-    )
-    REFERENCES `user` (
-                       `user_id`
-        );
-
-ALTER TABLE `community_board` ADD CONSTRAINT `FK_user_TO_community_board_1` FOREIGN KEY (
-                                                                                         `user_id`
-    )
-    REFERENCES `user` (
-                       `user_id`
-        );
-
-ALTER TABLE `community_reply` ADD CONSTRAINT `FK_community_board_TO_community_reply_1` FOREIGN KEY (
-                                                                                                    `community_id`
-    )
-    REFERENCES `community_board` (
-                                  `id`
-        );
-
-ALTER TABLE `community_reply` ADD CONSTRAINT `FK_user_TO_community_reply_1` FOREIGN KEY (
-                                                                                         `user_id`
-    )
-    REFERENCES `user` (
-                       `user_id`
-        );
-
-ALTER TABLE `qa_reply` ADD CONSTRAINT `FK_qa_board_TO_qa_reply_1` FOREIGN KEY (
-                                                                               `qa_id`
-    )
-    REFERENCES `qa_board` (
-                           `id`
-        );
-
-ALTER TABLE `qa_reply` ADD CONSTRAINT `FK_user_TO_qa_reply_1` FOREIGN KEY (
-                                                                           `user_id`
-    )
-    REFERENCES `user` (
-                       `user_id`
-        );
-
-ALTER TABLE `free_replay` ADD CONSTRAINT `FK_free_board_TO_free_replay_1` FOREIGN KEY (
-                                                                                       `free_id`
-    )
-    REFERENCES `free_board` (
+    REFERENCES `board_type` (
                              `id`
         );
 
-ALTER TABLE `free_replay` ADD CONSTRAINT `FK_user_TO_free_replay_1` FOREIGN KEY (
-                                                                                 `user_id`
-    )
-    REFERENCES `user` (
-                       `user_id`
-        );
+ALTER TABLE user ADD COLUMN `nickName` VARCHAR(100) NOT NULL;
 
-ALTER TABLE `enjoy_reply` ADD CONSTRAINT `FK_enjoy_board_TO_enjoy_reply_1` FOREIGN KEY (
-                                                                                        `enjoy_id`
-    )
-    REFERENCES `enjoy_board` (
-                              `id`
-        );
-
-ALTER TABLE `enjoy_reply` ADD CONSTRAINT `FK_user_TO_enjoy_reply_1` FOREIGN KEY (
-                                                                                 `user_id`
-    )
-    REFERENCES `user` (
-                       `user_id`
-        );
+ALTER TABLE help ADD COLUMN `help_type` INT NOT NULL;
