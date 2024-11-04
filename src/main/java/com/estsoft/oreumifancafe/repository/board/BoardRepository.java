54503b1ed246ad29d72ed876b30827177e0102de
ejjeong9103 <ejjeong9103@naver.com>
2024-11-04 13:15:37 +0900

    feat: BoardRepository 추가

---
+++ src/main/java/com/estsoft/oreumifancafe/repository/board/BoardRepository.java
@@ -0,0 +1,12 @@
+package com.estsoft.oreumifancafe.repository.board;
+
+import com.estsoft.oreumifancafe.domain.board.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board,Long> {
+    List<Board> findByUserUserId(String userId);
+}
