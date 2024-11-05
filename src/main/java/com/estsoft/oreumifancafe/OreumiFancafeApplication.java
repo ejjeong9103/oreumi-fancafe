cdc3da05f3529741fd6ed4057973a0fddc39325f
WeeYoungSeok <dndrldzk@gmail.com>
2024-11-05 16:27:09 +0900

    fix: main EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/estsoft/oreumifancafe/OreumiFancafeApplication.java
+++ src/main/java/com/estsoft/oreumifancafe/OreumiFancafeApplication.java
@@ -2,8 +2,10 @@ package com.estsoft.oreumifancafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OreumiFancafeApplication {
 
 	public static void main(String[] args) {
