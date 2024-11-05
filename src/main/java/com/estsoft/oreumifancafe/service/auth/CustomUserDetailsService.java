01b156567b42741700da8f473b46fa0dc8e48159
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-11-05 13:34:56 +0900

    refactor: CustomUserDetailsService 코드 수정

--- src/main/java/com/estsoft/oreumifancafe/service/auth/CustomUserDetailsService.java
+++ src/main/java/com/estsoft/oreumifancafe/service/auth/CustomUserDetailsService.java
@@ -1,20 +1,21 @@
 package com.estsoft.oreumifancafe.service.auth;
 
-import com.estsoft.oreumifancafe.exceptions.UserNotFoundException;
 import com.estsoft.oreumifancafe.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class CustomUserDetailsService implements CustomUserDetailsServiceInterf {
+public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
-    // ID로 검색
+    // ID로 사용자 검색
     @Override
-    public UserDetails loadUsersById(String userId) throws UserNotFoundException {
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
         return userRepository.findByUserId(userId)
-                .orElseThrow(() -> new UserNotFoundException(userId));
+                .orElseThrow(() -> new UsernameNotFoundException(userId + "가 존재하지 않습니다."));
     }
 }
