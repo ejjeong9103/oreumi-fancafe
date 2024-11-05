c66c04a365560e6a21a42da953428933136eb9dc
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-11-06 03:03:04 +0900

    feat: 관리자 페이지 기능
    DTO 파일 1개 생성, 사용자 정보 조회 기능 추가

---
+++ src/main/java/com/estsoft/oreumifancafe/domain/dto/admin/UserInfoResponse.java
@@ -0,0 +1,21 @@
+package com.estsoft.oreumifancafe.domain.dto.admin;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfoResponse {
+    private String userId;
+    private String userPw;
+    private String nickname;
+    private String address;
+    private LocalDateTime createdAt;
+    private int role;
+    private int state;
+    private String profileImageAddress;
+}
--- src/main/java/com/estsoft/oreumifancafe/service/admin/AdminService.java
+++ src/main/java/com/estsoft/oreumifancafe/service/admin/AdminService.java
@@ -1,20 +1,54 @@
 package com.estsoft.oreumifancafe.service.admin;
 
+import com.estsoft.oreumifancafe.domain.dto.admin.UserInfoResponse;
+import com.estsoft.oreumifancafe.domain.dto.help.HelpResponse;
 import com.estsoft.oreumifancafe.domain.dto.user.UserResponse;
-import com.estsoft.oreumifancafe.domain.user.User;
+import com.estsoft.oreumifancafe.domain.help.Help;
+import com.estsoft.oreumifancafe.exceptions.UserNotFoundException;
+import com.estsoft.oreumifancafe.repository.help.HelpRepository;
 import com.estsoft.oreumifancafe.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class AdminService {
     private final UserRepository userRepository;
+    private final HelpRepository helpRepository;
 
-    // 유저 검색
+    // ID로 사용자 조회
     public UserResponse findUserById(String userId) {
-        Optional<User> user = userRepository.findByUserId(userId);
+        return userRepository.findByUserId(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId))
+                .toUserResponse();
+    }
+
+    // 신고 및 문의 전체 조회
+    public List<HelpResponse> getAllHelp(String userId) {
+        return helpRepository.findByUserId(userId).stream()
+                .map(HelpResponse::new)
+                .toList();
+    }
+
+    // 신고 및 문의 페이지 카운트
+    public Long helpPageCount(String userId) {
+        List<HelpResponse> helpList = getAllHelp(userId);
+
+        return (long) helpList.size();
+    }
+
+    // 게시글 전체 조회
+
+    // 특정 게시판 게시글 조회
+
+    // 모든 사용자 조회
+    
+    // 사용자 정보 조회
+    public UserInfoResponse userInfo(String userId) {
+        return userRepository.findByUserId(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId))
+                .toUserInfoResponse();
     }
 }
