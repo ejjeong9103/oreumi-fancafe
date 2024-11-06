14bc14a099c5c03678d075772d2d64576e676c38
YoonjongJ <yoonjong.jang.123@gmail.com>
2024-11-06 18:06:25 +0900

    rename: SecurityConfig 코드 변경

--- src/main/java/com/estsoft/oreumifancafe/config/auth/SecurityConfig.java
+++ src/main/java/com/estsoft/oreumifancafe/config/auth/SecurityConfig.java
@@ -1,15 +1,22 @@
 package com.estsoft.oreumifancafe.config.auth;
 
+import com.estsoft.oreumifancafe.aop.CustomAuthenticationFailureHandler;
+import com.estsoft.oreumifancafe.aop.CustomAuthenticationSuccessHandler;
 import com.estsoft.oreumifancafe.service.auth.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 @RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
     private final CustomUserDetailsService userDetailsService;
@@ -31,17 +38,22 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .authorizeHttpRequests(auth -> auth
-                        //.requestMatchers("/board/1/**").hasRole("GUEST")      // 준회원이 접근할 수 있는 페이지 설정
-                        //.requestMatchers("/board/2/**").hasRole("USER")       // 정회원
-                        //.requestMatchers("/board/3/**").hasRole("ELITE")      // 우수회원
-                        //.requestMatchers("/board/4/**").hasRole("CELEBRITY")  // 연예인
-                        //.requestMatchers("/").hasRole("ADMIN")      // 관리자
-                        //.requestMatchers("/").hasAnyRole("CELEBRITY", "ADMIN") // 연예인과 관리자가 접근할 수 있는 페이지 설정
+                        .requestMatchers("/user/login").permitAll()
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/").hasRole("GUEST")      // 준회원이 접근할 수 있는 페이지 설정
+                        .requestMatchers("/").hasRole("USER")       // 정회원
+                        .requestMatchers("/").hasRole("ELITE")      // 우수회원
+                        .requestMatchers("/").hasRole("CELEBRITY")  // 연예인
+                        .requestMatchers("/").hasRole("ADMIN")      // 관리자
+                        .requestMatchers("/").hasAnyRole("CELEBRITY", "ADMIN") // 연예인과 관리자가 접근할 수 있는 페이지 설정
                         .anyRequest().permitAll()
                 )
                 .formLogin(auth -> auth
-                        .loginPage("/login") // 로그인 페이지 URL
-                        .defaultSuccessUrl("/main") // 로그인 성공시 자동으로 이동할 페이지 URL
+                        .loginProcessingUrl("/user/login")
+                        .usernameParameter("userId")
+                        .passwordParameter("userPw")
+                        .successHandler(new CustomAuthenticationSuccessHandler())
+                        .failureHandler(new CustomAuthenticationFailureHandler())
                 )
                 .logout(auth -> auth
                         .logoutSuccessUrl("/logout") // 로그아웃 성공시 자동으로 이동할 페이지 URL
@@ -49,5 +61,14 @@ public class SecurityConfig {
                 )
                 .csrf(auth -> auth.disable()) // CSRF 비활성화
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, CustomUserDetailsService userDetailsService) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder);
+        return authenticationManagerBuilder.build();
     }
 }
