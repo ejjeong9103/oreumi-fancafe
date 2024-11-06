077a8ec10556b0642bd1d1932d4420ea959bd7ef
WeeYoungSeok <dndrldzk@gmail.com>
2024-11-06 16:23:37 +0900

    feat: 시큐리티 로그인시 아이디 비번 매칭 구현 (body)
    ROLE은 해당 구현이 끝난 후 테이블 추가해서 주석쪽 코드 추가 예정

--- src/main/java/com/estsoft/oreumifancafe/service/auth/CustomUserDetailsService.java
+++ src/main/java/com/estsoft/oreumifancafe/service/auth/CustomUserDetailsService.java
@@ -1,9 +1,11 @@
 package com.estsoft.oreumifancafe.service.auth;
 
+import com.estsoft.oreumifancafe.domain.user.User;
 import com.estsoft.oreumifancafe.exceptions.UserNotFoundException;
 import com.estsoft.oreumifancafe.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -16,5 +18,15 @@ public class CustomUserDetailsService implements CustomUserDetailsServiceInterf 
     public UserDetails loadUsersById(String userId) throws UserNotFoundException {
         return userRepository.findByUserId(userId)
                 .orElseThrow(() -> new UserNotFoundException(userId));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserId(username)
+                .orElseThrow(() -> new UsernameNotFoundException("아이디 또는 비밀번호를 확인해주세요."));
+
+        // 권한
+
+        return user;
     }
 }
--- src/main/java/com/estsoft/oreumifancafe/service/auth/CustomUserDetailsServiceInterf.java
+++ src/main/java/com/estsoft/oreumifancafe/service/auth/CustomUserDetailsServiceInterf.java
@@ -2,7 +2,8 @@ package com.estsoft.oreumifancafe.service.auth;
 
 import com.estsoft.oreumifancafe.exceptions.UserNotFoundException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface CustomUserDetailsServiceInterf {
+public interface CustomUserDetailsServiceInterf extends UserDetailsService {
     UserDetails loadUsersById(String userId) throws UserNotFoundException;
 }
