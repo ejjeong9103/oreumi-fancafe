5d93101fdf8b4d2e11f91d99ac47ae4cc4d8541e
WeeYoungSeok <dndrldzk@gmail.com>
2024-11-11 16:59:12 +0900

    feat: global exception 추가

--- src/main/java/com/estsoft/oreumifancafe/aop/GlobalExceptionHandler.java
+++ src/main/java/com/estsoft/oreumifancafe/aop/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.estsoft.oreumifancafe.aop;
 
+import com.estsoft.oreumifancafe.exceptions.ForbiddenException;
+import com.estsoft.oreumifancafe.exceptions.UnauthorizedException;
+import com.estsoft.oreumifancafe.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,5 +14,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     private ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler({UnauthorizedException.class, ForbiddenException.class, UserNotFoundException.class})
+    public String handleUnauthorizedException(UnauthorizedException ex,
+                                              Model model) {
+        model.addAttribute("message", ex.getMessage());
+        return "errorPage"; // 에러 페이지로 리다이렉트
     }
 }
