6320c5c149f45195a828f0790fe46859bc3c1461
cjy <cjiyoon1130@gmail.com>
2024-11-12 17:35:36 +0900

    feat : 관리자 권한 관련 기능

--- src/main/java/com/estsoft/oreumifancafe/controller/help/HelpPageController.java
+++ src/main/java/com/estsoft/oreumifancafe/controller/help/HelpPageController.java
@@ -6,6 +6,8 @@ import com.estsoft.oreumifancafe.domain.help.Help;
 import com.estsoft.oreumifancafe.domain.user.User;
 import com.estsoft.oreumifancafe.service.help.HelpService;
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ import java.util.stream.Collectors;
 
 @Controller
 public class HelpPageController {
+    private static final Logger log = LoggerFactory.getLogger(HelpPageController.class);
     private final HelpService service;
     private final HttpSession session;
 
@@ -33,17 +36,17 @@ public class HelpPageController {
     // GET - 내가 쓴 질문 단건 조회
     @GetMapping("/help/question/{id}")
     public String findQuestionById(@PathVariable long id, Model model) {
-        User currentUser=(User)session.getAttribute("user");
+        User currentUser = (User) session.getAttribute("user");
         Help question = service.findQuestionBy(id);
 
-        if(!question.getUserId().equals(currentUser.getUserId())){
-            model.addAttribute("alertMessage","해당 글을 볼 권한이 없습니다.");
+        if (!question.getUserId().equals(currentUser.getUserId()) && !currentUser.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"))) {
+            model.addAttribute("alertMessage", "해당 글을 볼 권한이 없습니다.");
             return "alert";
         }
 
         HelpResponse response = new HelpResponse(question);
         model.addAttribute("help", response);
-        model.addAttribute("user",currentUser);
+        model.addAttribute("user", currentUser);
         return "qnadetail"; // 반환할 템플릿 이름
     }
 
@@ -64,8 +67,8 @@ public class HelpPageController {
         // 임시 어드민
         question.setAdminId("admin");
         model.addAttribute("help", question);
-        model.addAttribute("title",question.getTitle());
-        model.addAttribute("helpType",question.getHelpType());
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("helpType", question.getHelpType());
         return "qnaEditor";
     }
 
@@ -91,7 +94,7 @@ public class HelpPageController {
     // 유저 - 내가 쓴 문의글/신고글 조회(하나의 페이지, 여러 게시판)
     @GetMapping("/myPage")
     public String findHelpByUser(Model model) {
-        User currentUser=(User)session.getAttribute("user");
+        User currentUser = (User) session.getAttribute("user");
 
         List<HelpResponse> inquiryList = service.findInquiryByUserId(currentUser.getUserId())
                 .stream()
