127f5b37da7d5cb7044c667814ec4232df2a8118
WeeYoungSeok <dndrldzk@gmail.com>
2024-11-12 16:10:29 +0900

    feat: 게시판 목록 프론트 구현

--- src/main/java/com/estsoft/oreumifancafe/controller/HomeController.java
+++ src/main/java/com/estsoft/oreumifancafe/controller/HomeController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String index() {
+        // 메인 페이지 게시판 목록 불러오기
+        // 추후 리펙토링
+        // 게시판 type의 테이블을 만들어 해당 테이블을 조회해서
+        // nav에서 사용
         return "index";
     }
 }
